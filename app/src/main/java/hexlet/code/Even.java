663d83a2f662b902ab3a0ae4f95a815cbc6c8196
V. Shubina <victoriya.shubina.mex@gmail.com>
2023-10-07 19:27:05 +0300

    continue to build even

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -1,9 +1,25 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 class Even {
     static void play() {
+        int num = getRandomNumber();
+        Scanner scannedAnswer = new Scanner(System.in);
         Cli.greet();
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-        System.out.print("Question: ");
+        System.out.println("Question: " + num);
+        System.out.print("Your answer: ");
+        String answer = scannedAnswer.next();
+    }
+    static int getRandomNumber() {
+        int num = Math.random() * 100000;
+        return num;
+    }
+    static String isEven(int num) {
+        if (num % 2 == 0) {
+            return "yes";
+        }
+        return "no";
     }
 }
