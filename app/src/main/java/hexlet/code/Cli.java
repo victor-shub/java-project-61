d4b73024e7c0efaef5155fa5c0d2fb4a5328ebb0
V. Shubina <victoriya.shubina.mex@gmail.com>
2023-10-07 19:07:07 +0300

    add even game's base

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,7 +3,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 class Cli {
-    public static void Greet() {
+    public static void greet() {
         Scanner scannedName = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
---
+++ app/src/main/java/hexlet/code/Even.java
@@ -0,0 +1,9 @@
+package hexlet.code;
+
+class Even {
+    static void play() {
+        Cli.greet();
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        System.out.print("Question: ");
+    }
+}
