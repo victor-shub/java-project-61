11f2e5a371206f27c4b87d38e33316eaaa5129a4
V. Shubina <victoriya.shubina.mex@gmail.com>
2023-10-12 13:31:43 +0300

    complete gcd

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -1,18 +1,27 @@
 package hexlet.code.games;
 
+import java.math.BigInteger;
 import hexlet.code.Game;
 import hexlet.code.Engine;
 
 public class GCD implements Game {
+    private int firstNum;
+    private int secondNum;
     public static void play() {
         GCD game = new GCD();
         var task = "Find the greatest common divisor of given numbers.";
         Engine.getGameBody(game, task);
     }
     public String getQuestion() {
-        return null;
+        this.firstNum = Even.getRandomNumber();
+        this.secondNum = Even.getRandomNumber();
+        var question = firstNum + " " + secondNum;
+        return question;
     }
     public String getAnswer() {
-        return null;
+        var b1 = BigInteger.valueOf(firstNum);
+        var b2 = BigInteger.valueOf(secondNum);
+        var gcd = b1.gcd(b2);
+        return "" + gcd.intValue();
     }
 }
