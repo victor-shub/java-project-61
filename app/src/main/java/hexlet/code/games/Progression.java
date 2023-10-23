aba01bdfe8e5f7fac7e81761b903b9321b1eff02
V. Shubina <victoriya.shubina.mex@gmail.com>
2023-10-23 18:54:26 +0300

    improve generateProgression

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -4,7 +4,7 @@ import hexlet.code.Engine;
 import hexlet.code.Utils;
 
 public final class Progression {
-    private static final int seqLength = 10;
+    private static final int gameSeqLength = 10;
     private static final int lowFirstNumBound = -100;
     private static final int highFirstNumBound = 100;
     private static final int lowStepBound = -10;
@@ -18,10 +18,12 @@ public final class Progression {
         String[][] questionsAndAnswers = new String[2][Engine.QUESTIONS_COUNT];
         var skippedSeqNum = 0;
         for (var i = 0; i < Engine.QUESTIONS_COUNT; i++) {
-            int[] seqNums = generateRandomProgression();
-            var skipNum = Utils.generateRandomInt(0, seqLength);
+            var seqNum = Utils.generateRandomInt(lowFirstNumBound, highFirstNumBound);
+            var stepNum = Utils.generateRandomInt(lowStepBound, highStepBound);
+            int[] seqNums = generateProgression(seqNum, stepNum, gameSeqLength);
+            var skipNum = Utils.generateRandomInt(0, gameSeqLength);
             var question = new StringBuilder();
-            for (var j = 0; j < seqLength; j++) {
+            for (var j = 0; j < gameSeqLength; j++) {
                 if (j == skipNum) {
                     question.append(".. ");
                     skippedSeqNum = seqNums[j];
@@ -35,10 +37,8 @@ public final class Progression {
         }
         return questionsAndAnswers;
     }
-    static int[] generateRandomProgression() {
+    static int[] generateProgression(int seqNum, int stepNum, int seqLength) {
         int[] progression = new int[seqLength];
-        var seqNum = Utils.generateRandomInt(lowFirstNumBound, highFirstNumBound);
-        var stepNum = Utils.generateRandomInt(lowStepBound, highStepBound);
         for (var i = 0; i < seqLength; i++) {
             progression[i] = seqNum;
             seqNum += stepNum;
